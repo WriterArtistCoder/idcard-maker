9997f44fd2648ab6d82ca44974806803f605d7f4
league <league@imac-18.attlocal.net>
2021-09-19 16:38:08 -0700

    Fixed com.google.zxing dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module crypto {
     exports crpyto;
+	requires com.google.zxing;
+	requires com.google.zxing.javase;
 }
-"You're a wizard, Harry," rumbled Hagrid.
-Harry blinked. "I'm a what?"
-Hagrid stood up sharply. "A wizard, Harry. You got magic from the tip of your nose to the soles of your toes. From the pupils of your eyes to the roots of your hair."
-"Wow."
--- src/main/java/qr/Generator.java
+++ src/main/java/qr/Generator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
@@ -12,6 +13,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 
 public class Generator {
 
