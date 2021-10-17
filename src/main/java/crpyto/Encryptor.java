123745269dbb9f015f902f06f372c02311cc95ad
WriterArtistCoder <soda.law@gmail.com>
2021-10-17 16:29:21 -0700

    Adapted for longer hex strings
    And moved keys to text file

--- src/main/java/crpyto/Encryptor.java
+++ src/main/java/crpyto/Encryptor.java
@@ -1,12 +1,11 @@
 package crpyto;
 
-import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Encryptor {
 	public static void main(String[] args) throws NoSuchAlgorithmException {
-		byte[][] e = encrypt(0xce7, 0x9f426b);
+		byte[][] e = encrypt("ce7", "9f426b");
 
 		System.out.println("Front");
 		for (byte b : e[0]) {
@@ -22,39 +21,65 @@ public class Encryptor {
 	/**
 	 * Returns a 2D array of two byte arrays each containing hashes. The first
 	 * element should be used as the raw data of the front QR code, the second as
-	 * the back.
+	 * the back. The personal key should be twice the length of the organization
+	 * key.
 	 * 
-	 * @param okey Organization key
-	 * @param pkey Personal key (length must be double org. key)
+	 * @param oKey Organization key as hex string
+	 * @param pKey Personal key as hex string
 	 * @return [Front hash, back hash]
 	 * @throws NoSuchAlgorithmException
 	 */
-	public static byte[][] encrypt(int okey, int pkey) throws NoSuchAlgorithmException {
+	public static byte[][] encrypt(String oKey, String pKey) throws NoSuchAlgorithmException {
 		// Divide into two halves
-		String okeys = Integer.toHexString(okey);
-		String pkeys = Integer.toHexString(pkey);
-		int fInt = Integer.parseInt(okeys + pkeys.substring(0, pkeys.length() / 2), 16);
-		int bInt = Integer.parseInt(okeys + pkeys.substring(pkeys.length() / 2), 16);
+		String sxFront = oKey + pKey.substring(0, pKey.length() / 2);
+		String sxBack = oKey + pKey.substring(pKey.length() / 2);
 
-		System.out.println(fInt + " " + bInt);
+		System.out.println("Organization key: " + oKey);
+		System.out.println("Personal key:     " + pKey);
+		System.out.println("\nUNENCRYPTED");
+		System.out.println("Front QR data:    " + sxFront);
+		System.out.println("Back QR data:     " + sxBack);
 
 		// Hash
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		byte[] fByte = digest.digest(intToBytes(fInt));
-		byte[] bByte = digest.digest(intToBytes(bInt));
+		byte[] bFront = digest.digest(hexToBytes(sxFront));
+		byte[] bBack = digest.digest(hexToBytes(sxBack));
 
-		return new byte[][] { fByte, bByte };
+		System.out.println("\nENCRYPTED");
+		System.out.println("Front QR data:    " + bytesToHex(bFront));
+		System.out.println("Back QR data:     " + bytesToHex(bBack));
+
+		return new byte[][] { bFront, bBack };
 	}
 
 	/**
-	 * Converts an int to a byte array.
+	 * Converts a hexadecimal string to a byte array.
 	 * 
-	 * @param i Input int
+	 * @param hex Hex string
 	 * @return Byte array
 	 */
-	private static byte[] intToBytes(final int i) {
-		ByteBuffer bb = ByteBuffer.allocate(4);
-		bb.putInt(i);
-		return bb.array();
+	private static byte[] hexToBytes(String hex) {
+		int len = hex.length();
+		byte[] bytes = new byte[len / 2];
+
+		for (int i = 0; i < len; i += 2) {
+			bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character.digit(hex.charAt(i + 1), 16));
+		}
+		return bytes;
+	}
+
+	/**
+	 * Converts a byte array to a hexadecimal string.
+	 * 
+	 * @param bytes Byte array 
+	 * @return Hex string
+	 */
+	private static String bytesToHex(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		for (byte b : bytes) {
+			sb.append(String.format("%02x", b));
+		}
+
+		return sb.toString();
 	}
 }
--- src/main/java/qr/Generator.java
+++ src/main/java/qr/Generator.java
@@ -1,9 +1,17 @@
+package qr;
+
 // Java code to generate QR code
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
@@ -16,36 +24,52 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import crpyto.Encryptor;
 
 public class Generator {
+	private static String[] keys;
+
+	static {
+		ArrayList<String> alKeys = new ArrayList<>();
+		try {
+			File file = new File("src/main/resources/keys.txt");
+			Scanner reader = new Scanner(file);
+			while (reader.hasNextLine()) {
+				alKeys.add(reader.nextLine());
+			}
+			
+			reader.close();
+	        keys = new String[alKeys.size()];
+	        keys = alKeys.toArray(keys);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 	// Function to create the QR code
-	public static void createQR(String data, String path,
-								String charset, Map hashMap,
-								int height, int width)
-		throws WriterException, IOException
-	{
-		BitMatrix matrix = new MultiFormatWriter().encode(
-			new String(data.getBytes(charset), charset),
-			BarcodeFormat.QR_CODE, width, height);
+	public static void createQR(String data, String path, String charset, Map hashMap, int height, int width)
+			throws WriterException, IOException {
+		BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset),
+				BarcodeFormat.QR_CODE, width, height);
 
-		MatrixToImageWriter.writeToFile(
-			matrix,
-			path.substring(path.lastIndexOf('.') + 1),
-			new File(path));
+		MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
 	}
 
 	// Driver code
 	public static void main(String[] args)
 		throws WriterException, IOException,
-			NotFoundException
+			NotFoundException, NoSuchAlgorithmException
 	{
-		Encryptor e = new Encryptor();
+		// TODO improve docs, print keys
+		// The data that the QR code will contain
+		byte[][] exBoth = Encryptor.encrypt(keys[0], keys[1]);
+		
+		String e64Front = Base64.getUrlEncoder().encodeToString(exBoth[0]);
+		String e64Back = Base64.getUrlEncoder().encodeToString(exBoth[1]);
 
-		// The data that the QR code will containString originalInput = "test input";
-		String data = e.encrypt(0x111, 0x123456).getEncoder().encodeToString(originalInput.getBytes());
-		String data = e.encrypt(0x111, 0x123456);
+		System.out.println("\nENCRYPTED BASE-64");
+		System.out.println("Front QR data:    " + e64Front);
+		System.out.println("Back QR data:     " + e64Back);
 
 		// The path where the image will get saved
-		String path = "../resources/demo.png";
+		String path = "src/main/resources/demo.png";
 
 		// Encoding charset
 		String charset = "UTF-8";
@@ -60,7 +84,7 @@ public class Generator {
 		// Create the QR code and save
 		// in the specified folder
 		// as a jpg file
-		createQR(data, path, charset, hashMap, 200, 200);
+		createQR(e64Front, path, charset, hashMap, 200, 200);
 		System.out.println("QR Code Generated!!! ");
 	}
 }
